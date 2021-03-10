6bae0baba627c9c1fdfeb75052e41b41121398ea
Manuel Klettner <manuel.klettner@gmx.de>
2021-03-11 00:28:30 +0100

    Fix copy past color

--- mm_remote/lib/shared/styles.dart
+++ mm_remote/lib/shared/styles.dart
@@ -6,19 +6,24 @@ class Styles {
   static ThemeData themeData(bool isDarkTheme, BuildContext context) {
     isDarkTheme ? setDarkMode() : setLightMode();
     return ThemeData(
-      primaryColor: primaryColor,
-      hintColor: tertiaryColorDark,
-      cardColor: cardBackgroundColor,
-      focusColor: accentColor,
-      backgroundColor: backgroundColor,
-      dividerColor: highlightColor,
-      dialogBackgroundColor: secondaryBackgroundColor,
-      iconTheme: IconThemeData(color: tertiaryColorMedium),
-      textButtonTheme: TextButtonThemeData(
-        style: TextButton.styleFrom(
-          primary: tertiaryColorDark,
+        brightness: isDarkTheme ? Brightness.dark : Brightness.light,
+        primaryColor: primaryColor,
+        hintColor: tertiaryColorDark,
+        cardColor: cardBackgroundColor,
+        focusColor: accentColor,
+        backgroundColor: backgroundColor,
+        dividerColor: highlightColor,
+        dialogBackgroundColor: secondaryBackgroundColor,
+        iconTheme: IconThemeData(color: tertiaryColorMedium),
+        floatingActionButtonTheme:
+            FloatingActionButtonThemeData(foregroundColor: Colors.white),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            primary: tertiaryColorDark,
+          ),
         ),
-      ),
-    );
+        textSelectionTheme: TextSelectionThemeData(
+          selectionHandleColor: Colors.blue[400],
+        ));
   }
 }
