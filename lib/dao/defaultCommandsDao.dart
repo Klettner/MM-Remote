abec116f87788b27bb7cffceb3e8139fcbace8c8
Manuel Klettner <manuel.klettner@gmx.de>
2021-03-13 21:52:13 +0100

    Delete debug output

--- lib/dao/defaultCommandsDao.dart
+++ lib/dao/defaultCommandsDao.dart
@@ -9,13 +9,9 @@ void persistDefaultCommands(
   });
   Hive.box('defaultCommands3').delete(deviceName);
 
-  print("persisted DefaultCommands: ");
-  print(defaultCommandStrings);
   Hive.box('defaultCommands3').put(deviceName, defaultCommandStrings);
 }
 
 List<String> getDefaultCommands(String deviceName) {
-  print("returned DefaultCommands");
-  print(Hive.box('defaultCommands3').get(deviceName));
   return Hive.box('defaultCommands3').get(deviceName);
 }
